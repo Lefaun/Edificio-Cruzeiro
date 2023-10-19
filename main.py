503cab7c87ca000ee7f71f6b0be1c3b8b51ad38d
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-10-19 11:23:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,29 +21,24 @@ add_bg_from_local('D2C15DDC-4A94-4AC5-8A6C-DF8F6D571B90_1_105_c.jpeg')
 
 st.title("Edficio Cruzeiro")
 st.header("Plantas dos Apartamentos")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
    st.header("Apartamento T1 RC ESQ")
-   def show_pdf(file_path):
-            with open("T1 RC ESQ.pdf", "rb") as f:
-                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+   st.image("T1 RC DTO.PNG.png")
 
 with col2:
    st.header("Apartamento T1 RC Dto")
-   def show_pdf(file_path):
-            with open("T1 RC DTO.pdf", "rb") as f:
-                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+   st.image("T1 RC FTE.png")
 
+with col3:
+   st.header("Apartamento T1 RC Dto")
+   st.image("T3 RC ESQ.png")
 
 st.header("Apartamento T1 RC FTE")
 def show_pdf(file_path):
         with open("T1 RC FTE.pdf", "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="200" height="200" type="application/pdf"></iframe>'
 st.markdown(pdf_display, unsafe_allow_html=True)
 
 
