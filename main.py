8d63bf9e77d069d84307097cacfc2c0744ab36ad
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-10-19 11:04:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ add_bg_from_local('D2C15DDC-4A94-4AC5-8A6C-DF8F6D571B90_1_105_c.jpeg')
 
 st.title("Edficio Cruzeiro")
 st.header("Plantas dos Apartamentos")
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
    st.header("Apartamento T1 RC ESQ")
    def show_pdf(file_path):
             with open("T1 RC ESQ.pdf", "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
 
 with col2:
@@ -35,28 +35,28 @@ with col2:
    def show_pdf(file_path):
             with open("T1 RC DTO.pdf", "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
 
-with col3:
-   st.header("Modelação Urbana")
-   def show_pdf(file_path):
-            with open("T1 RC FTE.pdf", "rb") as f:
-                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-            st.markdown(pdf_display, unsafe_allow_html=True)
+
+st.header("Apartamento T1 RC FTE")
+def show_pdf(file_path):
+        with open("T1 RC FTE.pdf", "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 video_file = open('video4.mp4', 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
-col1, col2 = st.columns(2)
 
-with col1:
-   components.iframe("https://v3d.net/oeh", width=700, height=600)
-   st.markdown(components.iframe, unsafe_allow_html=True)
 
-with col2:
-   components.iframe("https://v3d.net/oi0", width=700, height=600)
-   st.markdown(components.iframe, unsafe_allow_html=True)
+
+components.iframe("https://v3d.net/oeh", width=700, height=600)
+st.markdown(components.iframe, unsafe_allow_html=True)
+
+
+components.iframe("https://v3d.net/oi0", width=700, height=600)
+st.markdown(components.iframe, unsafe_allow_html=True)
