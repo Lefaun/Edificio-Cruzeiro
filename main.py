083d41e1ee8922d8d0975b3ddce83b8d960ba433
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-10-19 12:07:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ add_bg_from_local('D2C15DDC-4A94-4AC5-8A6C-DF8F6D571B90_1_105_c.jpeg')
 st.title("Edficio Cruzeiro")
 st.header("Plantas dos Apartamentos")
 col1, col2, col3 = st.columns(3)
-with col1:
-   st.header("Apartamento T1 RC ESQ")
+with col3:
+   st.header("Apartamento T1 RC DTO")
    st.image("T1 RC DTO.PNG.png")
 
 with col2:
-   st.header("Apartamento T1 RC Dto")
+   st.header("Apartamento T1 RC FTE")
    st.image("T1 RC FTE.png")
 
-with col3:
-   st.header("Apartamento T1 RC Dto")
+with col1:
+   st.header("Apartamento T1 RC ESQ")
    st.image("T3 RC ESQ.png")
 
 col1, col2, col3 = st.columns(3)
@@ -53,6 +53,22 @@ video_bytes = video_file.read()
 
 st.video(video_bytes)
 
+col1, col2 = st.columns(2)
+with col1:
+   st.header("Pornenores da Construção")
+   st.image("")
+
+with col2:
+   st.header("Quarto Com Suite")
+   st.image("")
+
+col1, col2 = st.columns(2)
+with col1:
+   st.header(" Varanda Panoramica")
+   st.image("")
+with col2:
+   st.header("Exceentes Acabamentos")
+   st.image("")
 
 col1, col2 = st.columns(2)
 with col1:
